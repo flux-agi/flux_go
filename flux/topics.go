0fc70879a8006b55389828025431f3b1f71460f8
Zhogal Konstantin <k.zhogal@atomgroup.io>
2025-02-10 15:16:00 +0300

    BREAKING CHANGE: use dot symbol for topics names

--- flux/topics.go
+++ flux/topics.go
@@ -11,26 +11,26 @@ func NewTopics(service string) *ServiceTopics {
 }
 
 func (t *ServiceTopics) RequestConfig() string {
-	return fmt.Sprintf("service/%s/get_config", t.service)
+	return fmt.Sprintf("service.%s.get_config", t.service)
 }
 func (t *ServiceTopics) ResponseConfig() string {
-	return fmt.Sprintf("service/%s/set_config", t.service)
+	return fmt.Sprintf("service.%s.set_config", t.service)
 }
-func (t *ServiceTopics) SendStatus() string { return fmt.Sprintf("service/%s/status", t.service) }
+func (t *ServiceTopics) SendStatus() string { return fmt.Sprintf("service.%s.status", t.service) }
 func (t *ServiceTopics) RequestStatus() string {
-	return fmt.Sprintf("service/%s/request_status", t.service)
+	return fmt.Sprintf("service.%s.request_status", t.service)
 }
-func (t *ServiceTopics) Start() string   { return fmt.Sprintf("service/%s/start", t.service) }
-func (t *ServiceTopics) Stop() string    { return fmt.Sprintf("service/%s/stop", t.service) }
-func (t *ServiceTopics) Restart() string { return fmt.Sprintf("service/%s/restart", t.service) }
+func (t *ServiceTopics) Start() string   { return fmt.Sprintf("service.%s.start", t.service) }
+func (t *ServiceTopics) Stop() string    { return fmt.Sprintf("service.%s.stop", t.service) }
+func (t *ServiceTopics) Restart() string { return fmt.Sprintf("service.%s.restart", t.service) }
 func (t *ServiceTopics) PushDevelopmentMode(guid string) string {
-	return "service/development_mode/" + guid
+	return "service.development_mode." + guid
 }
-func (t *ServiceTopics) Errors() string     { return "service/*/error" }
-func (t *ServiceTopics) GlobalTick() string { return "service/tick" }
+func (t *ServiceTopics) Errors() string     { return "service." + ".error" }
+func (t *ServiceTopics) GlobalTick() string { return "service.tick" }
 func (t *ServiceTopics) GetCommonState() string {
-	return fmt.Sprintf("service/%s/get_common_state", t.service)
+	return fmt.Sprintf("service.%s.get_common_state", t.service)
 }
 func (t *ServiceTopics) SetCommonState() string {
-	return fmt.Sprintf("service/%s/set_common_state", t.service)
+	return fmt.Sprintf("service.%s.set_common_state", t.service)
 }
